09b11c858b2416f7352104bc5b4dfce67a554a8e
Matteo Possamai <mpossamaim@gmail.com>
2026-09-24 12:32:22 +0100

    Base engine and main

---
+++ 4_order_book_proj/src/main.cpp
@@ -0,0 +1,16 @@
+#include "engine.cpp"
+#include "messages.hpp"
+#include "queues.hpp"
+
+#define MAX_QUEUE_SIZE 2048
+#define MAX_ORDER_BUFFER_SIZE 2048
+#define ALLOCATOR_SIZE 2048
+
+int main() {
+  mpsc_queue<OUCHMessageIn, MAX_QUEUE_SIZE> incoming_queue;
+  spsc_queue<OUCHMessageOut, MAX_QUEUE_SIZE> outgoing_queue;
+  auto engine = Engine<MAX_QUEUE_SIZE, MAX_ORDER_BUFFER_SIZE, ALLOCATOR_SIZE>(
+      incoming_queue, outgoing_queue);
+  engine.top_of_book();
+  return 0;
+}
